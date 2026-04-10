8c776895d4cea66c020c0a5a91c4e511b389907c
Ganesh Ramchandra <aiexploreradventurerwonder@gmail.com>
2026-04-11 01:35:47 +0530

    Update pip install command in startup.sh
    Removed 'torch' from the pip install command in startup.sh.

--- startup.sh
+++ startup.sh
@@ -111,7 +111,7 @@ fi
 sleep 3
 export SOULX_SINGER_ROOT=/workspace/runpod-slim/ComfyUI/pretrained_models
 export PYTHONPATH=/workspace/runpod-slim/ComfyUI/custom_nodes/ComfyUI-SoulX-Singer:$PYTHONPATH
-$PYTHON -m pip install --upgrade torch soundfile librosa numpy omegaconf || true
+$PYTHON -m pip install --upgrade soundfile librosa numpy omegaconf || true
 
 
 # 🚀 Start ComfyUI
